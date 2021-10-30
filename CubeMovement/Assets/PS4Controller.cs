b807ccc6884dd96a2d21f9b29761d2c4b6163cba
RBG <roundbeargames@gmail.com>
2021-10-31 05:51:14 +0900

    save

---
+++ CubeMovement/Assets/PS4Controller.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace RB
+{
+    public class PS4Controller : MonoBehaviour
+    {
+        GameObject cube = null;
+
+        private void Start()
+        {
+            for (int i = 0; i < Gamepad.all.Count; i++)
+            {
+                Debug.Log(Gamepad.all[i].name);
+            }
+
+            cube = GameObject.Find("Cube");
+        }
+
+        private void Update()
+        {
+            if (Gamepad.all[0].leftStick.left.isPressed)
+            {
+                cube.transform.position += Vector3.left * Time.deltaTime * 5f;
+            }
+
+            if (Gamepad.all[0].leftStick.right.isPressed)
+            {
+                cube.transform.position += Vector3.right * Time.deltaTime * 5f;
+            }
+
+            if (Gamepad.all[0].leftStick.up.isPressed)
+            {
+                cube.transform.position += Vector3.up * Time.deltaTime * 5f;
+            }
+
+            if (Gamepad.all[0].leftStick.down.isPressed)
+            {
+                cube.transform.position += Vector3.down * Time.deltaTime * 5f;
+            }
+        }
+    }
+}
